35ddccc50024d6647e8c27eb0fb1218c1224f5f1
lgdiaz-usc <liamdiaz41@gmail.com>
2025-08-04 02:56:13 -0400

    Implemented assigning values and displaying them in hex and binary

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,95 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::fmt;
+
+struct BigInt {
+    inner: Vec<u8>
+}
+
+impl BigInt {
+    fn new_16(input: &str) -> Self {
+        let mut data = Vec::new();
+
+        let mut input_iter = input
+            .chars()
+            .rev()
+            .map(|c| {
+                if let Some(hex_digit) = c.to_digit(16) {
+                    hex_digit as u8
+                }
+                else {
+                    panic!("ERROR: Invalid Hexdigit \'{}\'", c);
+                }
+            });
+
+        while let Some(hex_digit_0) = input_iter.next() {
+            if let Some(hex_digit_1) = input_iter.next() {
+                data.push(hex_digit_0 | (hex_digit_1 << 4));
+            }
+            else {
+                data.push(hex_digit_0);
+            }
+        }
+
+        Self {
+            inner: data
+        }
+    }
+}
+
+impl fmt::Display for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "test")
+    }
+}
+
+impl fmt::Binary for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut inner_iter = self.inner.iter().rev();
+        let mut output = String::new();
+
+        if let Some(byte) = inner_iter.next() {
+            output.extend(format!("{:b}", byte).chars());
+        }
+        
+        for byte in inner_iter {
+            output.extend(format!("{:0>8b}", byte).chars());
+        }
+
+        f.pad_integral(true, "0b", &output)
+    }
+}
+
+impl fmt::UpperHex for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut inner_iter = self.inner.iter().rev();
+        let mut output = String::new();
+
+        if let Some(byte) = inner_iter.next() {
+            output.extend(format!("{:X}", byte).chars());
+        }
+        
+        for byte in inner_iter {
+            output.extend(format!("{:0>2X}", byte).chars());
+        }
+
+        f.pad_integral(true, "0X", &output)
+    }
+}
+
+impl fmt::LowerHex for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut inner_iter = self.inner.iter().rev();
+        let mut output = String::new();
+
+        if let Some(byte) = inner_iter.next() {
+            output.extend(format!("{:x}", byte).chars());
+        }
+        
+        for byte in inner_iter {
+            output.extend(format!("{:0>2x}", byte).chars());
+        }
+
+        f.pad_integral(true, "0x", &output)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +97,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_format() {
+        let format_works = BigInt::new_16("1A");
+        let binary_test = BigInt::new_16("1F1A");
+        let hex_test = BigInt::new_16("4A609");
+
+        assert_eq!(format!("{}", format_works), "test");
+        assert_eq!(format!("{:b}", binary_test), "1111100011010");
+        assert_eq!(format!("{:016b}", binary_test), "0001111100011010");
+        assert_eq!(format!("{:X}", hex_test), "4A609");
+        assert_eq!(format!("{:06X}", hex_test), "04A609");
+        assert_eq!(format!("{:x}", hex_test), "4a609");
+        assert_eq!(format!("{:06x}", hex_test), "04a609");
     }
 }
